4bd90e5c3ecc6781a1c1a900bdb926c00e3e7417
Ckott <diegoangeles130703@gmail.com>
2023-08-17 03:44:03 -0500

    Estadistica

--- ejercicio5.py
+++ ejercicio5.py
@@ -2,7 +2,7 @@
 
 def autoprint():
     print()
-    print("="*50)
+    print("="*45)
     print()
 
 print("-"*10,"ESTADISTA INDIFERENCIAL")
@@ -23,6 +23,12 @@ autoprint()
 
 
 # cuando no se conoce la poblacion
+
+autoprint()
+
+print("--- Cuando no se conoce la poblacion ---")
+autoprint()
+
 ## Formula
 #  
 #        Z x P x q 
@@ -30,27 +36,28 @@ autoprint()
 #          d**2
 #
 
+p:float = 0 #probabilidad de exito
+q:float = 0 #probabilidad de fracazo
+z:float = 0 #nivel de confianza / normalmente es de (1-α =0.95 -> z = 1.96)
+d:float = 0 #margen de error
+
+p = float(input("Ingresa - '(P)' - : "))
+q = float(input("Ingresa - '(q)' - : "))
+d = float(input("Ingresa - '(d) -o- (e)' - : "))
+z:float = 1.96
 autoprint()
 
+print(z,"² x",p,"x",q) 
+print("----------------------")
+print(d,"²")
 
-print("--- Cuando no se conoce la poblacion ---")
 autoprint()
 
-p:float = 0.85 #probabilidad de exito
-q:float = 0.15 #probabilidad de fracazo
-z:float = 1.96 #
-d:float = 0.05 
+n:float=0 
 
-n:float=0
-
-n = ((z)**2 * (p) * (q)) / d **2
-
-print(n)
-
-
-
-
-
+n = ((z)**2 * (p) * (q)) / d **2                                 
+print("Resultado : ",n)
+print("Redondeado : ",round(n))
 
 
 
