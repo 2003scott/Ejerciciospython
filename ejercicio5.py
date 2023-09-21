8e1588ad627310bc64ce200da301a0a1132ec652
Ckott <diegoangeles130703@gmail.com>
2023-09-21 02:18:25 -0500

    estadisca indiferencial

--- ejercicio5.py
+++ ejercicio5.py
@@ -44,42 +44,42 @@ print("Redondeado : ",round(r))
 
 
 
-# cuando no se conoce la poblacion
+# # cuando no se conoce la poblacion
 
-autoprint()
+# autoprint()
 
-print("--- Cuando no se conoce la poblacion ---")
-autoprint()
+# print("--- Cuando no se conoce la poblacion ---")
+# autoprint()
 
-## Formula
-#  
-#        Z x P x q 
-#   n= ------------
-#          d**2
-#
+# ## Formula
+# #  
+# #        Z x P x q 
+# #   n= ------------
+# #          d**2
+# #
 
-p:float = 0 #probabilidad de exito
-q:float = 0 #probabilidad de fracazo
-z:float = 0 #nivel de confianza / normalmente es de (1-α =0.95 -> z = 1.96)
-d:float = 0 #margen de error
+# p:float = 0 #probabilidad de exito
+# q:float = 0 #probabilidad de fracazo
+# z:float = 0 #nivel de confianza / normalmente es de (1-α =0.95 -> z = 1.96)
+# d:float = 0 #margen de error
 
-p = float(input("Ingresa - '(P)' - : "))
-q = float(input("Ingresa - '(q)' - : "))
-d = float(input("Ingresa - '(d) -o- (e)' - : "))
-z:float = 1.96
-autoprint()
+# p = float(input("Ingresa - '(P)' - : "))
+# q = float(input("Ingresa - '(q)' - : "))
+# d = float(input("Ingresa - '(d) -o- (e)' - : "))
+# z:float = 1.96
+# autoprint()
 
-print(z,"² x",p,"x",q) 
-print("----------------------")
-print(d,"²")
+# print(z,"² x",p,"x",q) 
+# print("----------------------")
+# print(d,"²")
 
-autoprint()
+# autoprint()
 
-n:float=0 
+# n:float=0 
 
-n = ((z**2) * (p) * (q)) / d **2                                 
-print("Resultado : ",n)
-print("Redondeado : ",round(n))
+# n = ((z**2) * (p) * (q)) / d **2                                 
+# print("Resultado : ",n)
+# print("Redondeado : ",round(n))
 
 
 
